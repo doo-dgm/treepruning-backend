60f0c73ca01faed8e21530f440da6870dfe77573
Michelgdr28 <michel.guarnizo@udea.edu.co>
2026-05-06 22:20:42 -0500

    .

--- src/main/java/co/edu/uco/treepruning/application/inputport/SubmitPQRInputPort.java
+++ src/main/java/co/edu/uco/treepruning/application/inputport/SubmitPQRInputPort.java
@@ -3,6 +3,6 @@ package co.edu.uco.treepruning.application.inputport;
 import co.edu.uco.treepruning.application.inputport.dto.SubmitPQRDTO;
 
 
-public interface SubmitPQRImputPort extends InputPort<SubmitPQRDTO, Void> {
+public interface SubmitPQRInputPort extends InputPort<SubmitPQRDTO, Void> {
 
 }
--- src/main/java/co/edu/uco/treepruning/application/inputport/impl/SubmitPQRInteractor.java
+++ src/main/java/co/edu/uco/treepruning/application/inputport/impl/SubmitPQRInteractor.java
@@ -1,14 +1,14 @@
 package co.edu.uco.treepruning.application.inputport.impl;
 
 
-import co.edu.uco.treepruning.application.inputport.SubmitPQRImputPort;
+import co.edu.uco.treepruning.application.inputport.SubmitPQRInputPort;
 import co.edu.uco.treepruning.application.inputport.dto.SubmitPQRDTO;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional(rollbackFor=Exception.class)
-public class SubmitPQRInteractor implements SubmitPQRImputPort {
+public class SubmitPQRInteractor implements SubmitPQRInputPort {
 
 
     @Override
