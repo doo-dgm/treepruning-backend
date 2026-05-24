5d5c54c4ff8604caaa09299190589c8d67fb1f28
zha808 <daniloc90@gmail.com>
2026-05-24 17:51:07 -0500

    Bean

--- src/main/java/co/edu/uco/treepruning/features/notification/registertoken/application/usecase/impl/mapper/NotificationTokenDomainMapper.java
+++ src/main/java/co/edu/uco/treepruning/features/notification/registertoken/application/usecase/impl/mapper/NotificationTokenDomainMapper.java
@@ -5,7 +5,7 @@ import org.mapstruct.Mapper;
 import co.edu.uco.treepruning.features.notification.registertoken.application.usecase.domain.NotificationTokenDomain;
 import co.edu.uco.treepruning.infrastructure.persistence.repository.entity.NotificationTokenEntity;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface NotificationTokenDomainMapper {
 	default NotificationTokenEntity toEntity(NotificationTokenDomain domain) {
 		if (domain == null) {
