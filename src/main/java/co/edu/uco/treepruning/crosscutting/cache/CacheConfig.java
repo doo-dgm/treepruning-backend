22d7aacfe953bfd2e88c40c6092131b6f4e16e6c
Michelgdr28 <michel.guarnizo@udea.edu.co>
2026-05-27 01:34:19 -0500

    cache...

--- src/main/java/co/edu/uco/treepruning/crosscutting/cache/CacheConfig.java
+++ src/main/java/co/edu/uco/treepruning/crosscutting/cache/CacheConfig.java
@@ -17,7 +17,7 @@ import java.util.Map;
 @Configuration
 public class CacheConfig {
 
-    @Bean
+    @Bean("cacheManager")
     @Primary
     public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
 
